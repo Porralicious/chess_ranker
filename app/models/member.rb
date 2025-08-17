7a205b919023861e7aff5a0a29b271693f132789
Porralicious <helder.jose.fernandes@gmail.com>
2025-08-17 15:43:48 +0200

    add delete cascade

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,14 @@
 class Member < ApplicationRecord
+    has_many :matches_as_player_one, 
+        class_name: "Match",            
+        foreign_key: "player_one_id", 
+        dependent: :destroy
+
+    has_many :matches_as_player_two, 
+        class_name: "Match", 
+        foreign_key: "player_two_id", 
+        dependent: :destroy
+        
     def full_name
         self.name + ' ' + self.surname
     end
